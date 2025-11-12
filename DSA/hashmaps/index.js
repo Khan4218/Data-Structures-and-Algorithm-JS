050406e08b467d9325fbdc16ba6c4bc653ef19f0
Akbar Khan <mirakber4@gmail.com>
2025-11-12 18:13:16 +0530

    set challenge 1

--- DSA/hashmaps/index.js
+++ DSA/hashmaps/index.js
@@ -17,25 +17,64 @@
 // Then loop again to find the first with count = 1.
 
 
-function nonRepeatingChar(str) {
+// function nonRepeatingChar(str) {
  
-    let frequencies = new Map()
+//     let frequencies = new Map()
     
-    for(let char of str) {        
-     frequencies.set(char,(frequencies.get(char)|| 0) + 1)
+//     for(let char of str) {        
+//      frequencies.set(char,(frequencies.get(char)|| 0) + 1)
    
-    }
+//     }
 
-    for(let char of str) {   
-      if(frequencies.get(char )=== 1) {
-        return char
-      }
-    }
+//     for(let char of str) {   
+//       if(frequencies.get(char )=== 1) {
+//         return char
+//       }
+//     }
     
+  
+// }
+
+// console.log(nonRepeatingChar("aabbcdee"));
+
+
+// 🧩 Challenge 2 — Check if Array Contains Duplicates
+
+// Problem:
+// Given an array of numbers, return true if any number appears more than once.
+
+// Example:
+
+// Input: [1, 2, 3, 1]
+// Output: true
+
+
+// Hint:
+
+// Use a Set to track seen numbers.
+
+// If a number already exists in the set, return true.
+
+function duplicates(arr) {
+ 
+   let set = new Set()
+
+
+   for(let num of arr) {
+     
+     if(set.has(num)) {
+     return true
+     }else{
+        set.add(num)
+     }
+ 
+    
+   }
+
+   return false
+
+  
   
 }
 
-console.log(nonRepeatingChar("aabbcdee"));
-
-
-
+console.log(duplicates([1, 2, 3, 1]));
