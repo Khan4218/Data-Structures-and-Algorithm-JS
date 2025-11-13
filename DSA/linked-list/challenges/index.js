e34b6cf811c88e19ad5a2848d6f7f3d8f4764a02
Akbar Khan <mirakber4@gmail.com>
2025-11-13 15:37:31 +0530

    challenge: Deleting Node

--- DSA/linked-list/challenges/index.js
+++ DSA/linked-list/challenges/index.js
@@ -21,84 +21,157 @@
 
 
 class Node{
-    constructor(data) {
-      this.data = data
-      this.next = null
-    }
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
 }
 
 class LinkedList{
-     constructor() {
-        this.head = null
-     }
+  constructor() {
+    this.head = null
+  }
+  
 
-     append(data) {
-        
-        const newNode = new Node(data)
-        
-        if(!this.head) {
-            this.head = newNode
-            return
-        }
-
-        let current = this.head
-        while(current.next) {
-          
-          current = current.next
-        }
-        current.next = newNode
-         
-    }
-
-    insertAtBeginning(data) {
-         const newNode = new Node(data)
-         newNode.next = this.head
-         this.head = newNode
-    }
-
-    insertAtPosition(data, position) {
-       const newNode = new Node(data)
-   
-       if(position === 0) {
-          newNode.next = this.head
+  append(data) {
+      
+      const newNode = new Node(data)
+      
+      if(!this.head) {
           this.head = newNode
-       }
+          return
+      }
 
-       let current = this.head
-       let count = 0
-
-       while(current && count < position - 1) { 
-         current = current.next
-         count++
-       }
-
-       if(!current) {
-        console.log("position out of bonds");
-        return
+      let current = this.head
+      while(current.next) {
         
-       }
+        current = current.next
+      }
+      current.next = newNode
+      
+  }
 
-       newNode.next = current.next
-       current.next = newNode
+  insertAtBeginning(data) {
+      const newNode = new Node(data)
+      newNode.next = this.head
+      this.head = newNode
+  }
 
+  insertAtPosition(data, position) {
+    const newNode = new Node(data)
+
+    if(position === 0) {
+      newNode.next = this.head
+      this.head = newNode
     }
 
+    let current = this.head
+    let count = 0
 
-     print() {
-        let current = this.head
-        while(current) {
-         console.log(current.data);
-        
-          current = current.next
-        }
-     }
+    while(current && count < position - 1) { 
+      current = current.next
+      count++
     }
+
+    if(!current) {
+      console.log("position out of bonds");
+      return
+      
+    }
+
+    newNode.next = current.next
+    current.next = newNode
+
+  
+  }
+
+  deleteAtBeginning() {
+
+    if(!this.head) return
+    this.head = this.head.next
+  }
+
+  deleteAtEnd() {
+
+    if(!this.head) return
+    
+    if(!this.head.next){
+     this.head = null
+     return
+    }
+
+    let current = this.head
+
+    while(current.next.next) {
+     
+      current = current.next
+    }
+
+    current.next = null
+  }
+
+  deleteAtPosition(position) {
+    
+    if(!this.head) return
+
+    if(position === 0) {
+      this.head = this.head.next
+      return
+    }
+
+    let current = this.head
+    let count = 0
+
+    while(current.next && count < position - 1) {
+      current = current.next
+      count++
+    }
+
+    if(!current.next) {
+      console.log("Position Out of bonds");
+      return;
+    }
+
+    current.next = current.next.next
+
+  }
+
+
+  
+
+
+  print() {
+    let current = this.head
+    while(current) {
+      console.log(current.data);
+    
+      current = current.next
+    }
+  }
+}
     
 const list = new LinkedList
 
 list.append(5)
 list.append(10)
 list.append(15)
-list.insertAtBeginning(2)
-list.insertAtPosition(3,2)
+list.append(3)
+list.append(7)
+
+console.log("Main");
 list.print()
+
+console.log("delete at beginning");
+list.deleteAtBeginning()
+list.print()
+
+console.log("delete at end")
+list.deleteAtEnd()
+list.print()
+
+console.log("Delete at position");
+list.deleteAtPosition(2)
+list.print()
+
+
+
