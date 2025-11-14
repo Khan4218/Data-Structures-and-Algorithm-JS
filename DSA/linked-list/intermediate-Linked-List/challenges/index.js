3cb9aba89c192040f09ef3d60272968f5085fe30
Akbar Khan <mirakber4@gmail.com>
2025-11-15 00:14:58 +0530

    challenge 4: print before and after reversal

--- DSA/linked-list/intermediate-Linked-List/challenges/index.js
+++ DSA/linked-list/intermediate-Linked-List/challenges/index.js
@@ -27,11 +27,7 @@ class LinkedList {
         current.next = newNode
         
     }
-    // Challenge 3: Reverse Empty List
-    // Input: null
-    // Task: Reverse the list iteratively.
-    // Expected output: null
-    // Edge case check: Ensure your code handles an empty list without errors.
+ 
     reverseEmptyList(){
      let next = null
      let prev = null
@@ -82,19 +78,25 @@ class LinkedList {
         }
     }
 }
+// Challenge 4: Print Before & After
+
+// Input: 1 → 2 → 3 → 4 → 5 → null
+
+// Task: Print the list before reversal, reverse it iteratively, then print it after reversal.
 
 const list = new LinkedList
-// list.append(5)
-// list.append(10)
-// list.append(15)
-// list.append(20)
-// console.log("Before Reverse");
-// list.print()
-// console.log("after Reverse");
-// list.reverse()
-// list.print()
-list.append(null)
-list.reverseEmptyList()
+list.append(1)
+list.append(2)
+list.append(3)
+list.append(4)
+list.append(5)
+console.log("Before Reverse");
 list.print()
+console.log("after Reverse");
+list.reverse()
+list.print()
+// list.append(null)
+// list.reverseEmptyList()
+// list.print()
 
 
