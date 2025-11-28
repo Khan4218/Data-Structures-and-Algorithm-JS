a9fe26a4b318c6a783ff68f2299ce485ffc05544
Akbar Khan <mirakber4@gmail.com>
2025-11-29 01:46:25 +0530

    challenge: BInary search tree

--- Scrimba-DSA/trees/Binary-search-trees/index.js
+++ Scrimba-DSA/trees/Binary-search-trees/index.js
@@ -25,10 +25,89 @@ function insert(root, value) {
 
 let root = null
 
+
+// Build the tree shown in your example
 root = insert(root, 6);
 root = insert(root, 4);
-root = insert(root, 10);
 root = insert(root, 8);
+root = insert(root, 3);
+root = insert(root, 5);
+root = insert(root, 7);
+root = insert(root, 9);
+root = insert(root, 1);
+
 
 console.log(JSON.stringify(root, null, 2));
 
+// Challenge:
+// **********
+
+// Implement the BST search operation. It will be similar to the insert operation 
+// you saw in the previous lesson. The function receives the root of the tree as well as the 
+// target value as inputs, and must return true if the target is found in the tree, and false if it’s not.
+
+
+// Examples:
+// **********
+
+// Example 1:
+// ```
+// root = ------->
+//               6
+//         4          8
+//      3     5    7     9
+//  1    
+
+// target = 5 
+// result = true
+// ```
+
+// Example 2:
+// ```
+// root = ------->
+//               6
+//         4          8
+//      3     5    7     9
+//  1
+
+// target = 15 
+// result = false
+// ```
+
+// Example 3:
+// ```
+// root = ------->
+//              null   
+
+// target = 15 
+// result = false
+// ```
+
+function search(root, target) {
+  
+    if(root === null) {
+      return false
+    }
+
+    if(root.value === target) {
+      return true
+    }
+
+    if(target < root.value) {
+     return search(root.left, target)
+    }
+
+    return search(root.right, target)
+}
+console.log(search(root, 5));   // true
+console.log(search(root, 15));  // false
+console.log(search(null, 15));  // false
+
+// Time Complexity:
+// Best: O(1)
+// Average: O(log n)
+// Worst: O(n)
+
+// Space Complexity:
+// Average: O(log n)
+// Worst: O(n)
